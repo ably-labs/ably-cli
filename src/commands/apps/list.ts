58927321fdfba0357dfd3dba644254c997be296e
Marc Duiker <marcduiker@gmail.com>
2022-10-27 15:30:23 +0200

    Add table output

--- src/commands/apps/list.ts
+++ src/commands/apps/list.ts
@@ -1,3 +1,4 @@
+import { CliUx } from '@oclif/core'
 import CommandWithGlobalConfig from '../command-with-global-config'
 
 export default class List extends CommandWithGlobalConfig {
@@ -10,13 +11,36 @@ export default class List extends CommandWithGlobalConfig {
 
   static flags = {
     ...CommandWithGlobalConfig.flags,
+    ...CliUx.ux.table.flags(),
   }
 
   async run(): Promise<void> {
     if (this.globalConfig === undefined) {
       this.log('Configuration not set')
     } else {
-      console.log((await this.ablyControlApi!.listApps()).map(app => app.name).sort())
+      const {flags} = await this.parse(List)
+      const apps = (await this.ablyControlApi!.listApps()).sort((a, b) => a.name.localeCompare(b.name))
+      CliUx.ux.table(apps as Record<string, any>[], {
+        name: {
+          header: 'Name',
+          minWidth: 25,
+        },
+        id: {
+          header: 'ID',
+          minWidth: 10,
+        },
+        status: {
+          header: 'Status',
+          minWidth: 10,
+        },
+        tlsOnly: {
+          header: 'TLS Only',
+          minWidth: 10,
+        },
+      }, {
+        printLine: this.log.bind(this),
+        ...flags,
+      })
     }
   }
 }
