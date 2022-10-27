f994ef92ddb857a68a7e9b3253c5c29fd32b8e2d
Marc Duiker <marcduiker@gmail.com>
2022-10-27 17:14:40 +0200

    Small text changes

--- src/commands/access/index.ts
+++ src/commands/access/index.ts
@@ -16,19 +16,19 @@ static hidden = false
   async run(): Promise<void> {
     this.log('To give the Ably CLI access to your account it needs two things:')
     this.log()
-    this.log('1. Your Ably Account ID')
-    this.log('2. An Access Token')
+    this.log('1. An Access Token')
+    this.log('2. Your Ably Account ID')
     this.log()
 
     this.log('Please follow these instructions to create an Access Token:')
     await CliUx.ux.url('https://ably.com/docs/control-api#authentication', 'https://ably.com/docs/control-api#authentication')
     this.log()
-    this.globalConfig.controlApiToken = await CliUx.ux.prompt('What is Control Access token?')
+    this.globalConfig.controlApiToken = await CliUx.ux.prompt('What is the Control API Access Token?')
 
     this.log('And the instructions here to get your Account ID:')
-    await CliUx.ux.url('https://ably.com/docs/control-api#ids', 'https://ably.com/docs/control-api#idsn')
+    await CliUx.ux.url('https://ably.com/docs/control-api#ids', 'https://ably.com/docs/control-api#ids')
     this.log()
-    this.globalConfig.accountId = await CliUx.ux.prompt('What is Account ID?')
+    this.globalConfig.accountId = await CliUx.ux.prompt('What is your Account ID?')
 
     this.saveGlobalConfig()
   }
